931e2bd73f608649e24654c801656db8dfcf2fdc
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-03 14:15:01 +0100

    Variablen_Lev1_6

---
+++ assets/Js/main.js
@@ -0,0 +1,13 @@
+
+/*let x = 34
+x = 67
+console.log(x); */
+
+const y = 34
+y = 67
+console.log(y);
+
+/* Die Variable x lässt sich mit let x überschreiben
+und die Variable y lässt sich mit const y nicht überschreiben*/
+
+
